b1c1dc8b290d75d7e21e5551a5475668646053b6
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-09-12 01:33:05 +0700

    add colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+export const COLORS = {
+  GREEN: '#008000',
+  GRAY: '#7A7A7A',
+  BLACK: '#000000',
+  WHITE: '#FFFFFF',
+  GRAY_SMOKE: '#F0F0F0',
+  NEUTRAL: '#D6D6D6',
+  RED: '#E94948',
+  LIGHT_GREY: '#C4C4C4',
+};
