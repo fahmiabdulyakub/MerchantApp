0acf48ac3446de66a6a144accf5c392e8105f283
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-09-12 00:13:27 +0700

    add fonts

---
+++ src/constants/fonts.ts
@@ -0,0 +1,9 @@
+export const FONTS = {
+  MontserratBlack: 'Montserrat-Black',
+  MontserratBold: 'Montserrat-Bold',
+  MontserratLight: 'Montserrat-Light',
+  MontserratMedium: 'Montserrat-Medium',
+  MontserratRegular: 'Montserrat-Regular',
+  MontserratSemiBold: 'Montserrat-SemiBold',
+  MontserratThin: 'Montserrat-Thin',
+};
