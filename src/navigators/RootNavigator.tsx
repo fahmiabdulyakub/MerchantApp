f2e087e1edcee0871a66e7e86bb33b55537afb9f
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-09-12 01:36:43 +0700

    create RootNavigator

---
+++ src/navigators/RootNavigator.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import BottomTabNavigator from '@navigators/BottomTabNavigator';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {COLORS} from '@constants/colors';
+import {AppParamList} from '@lib/navigation/types';
+import {RouteNames} from '@lib/navigation/routes';
+import {OTP, SignIn, Splash} from '@screens';
+import {navigationRef} from '@lib/navigation/utils';
+
+const Stack = createNativeStackNavigator<AppParamList>();
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <SafeAreaView style={styles.safeAreaView}>
+        <StatusBar backgroundColor={COLORS.WHITE} barStyle="dark-content" />
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}
+          initialRouteName={RouteNames.SPLASH}>
+          <Stack.Screen name={RouteNames.SPLASH} component={Splash} />
+          <Stack.Screen
+            name={RouteNames.MAIN_APP}
+            component={BottomTabNavigator}
+          />
+          <Stack.Screen name={RouteNames.SIGN_IN} component={SignIn} />
+          <Stack.Screen name={RouteNames.OTP} component={OTP} />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+});
