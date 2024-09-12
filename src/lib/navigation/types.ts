799dc2dcf10150490f044c3a7cfbe261bfba2fcd
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-09-13 01:58:54 +0700

    add params OTP

--- src/lib/navigation/types.ts
+++ src/lib/navigation/types.ts
@@ -1,10 +1,11 @@
 import {RouteNames} from '@lib/navigation/routes';
+import {IOTP} from '@screens/OTP/types';
 
 export type AppParamList = {
   [RouteNames.HOME]: undefined;
   [RouteNames.MAIN_APP]: undefined;
   [RouteNames.MANAGE]: undefined;
-  [RouteNames.OTP]: undefined;
+  [RouteNames.OTP]: IOTP;
   [RouteNames.PROFILE]: undefined;
   [RouteNames.SHOP]: undefined;
   [RouteNames.SIGN_IN]: undefined;
