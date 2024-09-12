b8a2385017b0185e87ca5b18c58f1bab0b54f5b9
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-09-13 01:58:09 +0700

    add ICArrowLeft

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,7 @@
+import ICArrowLeft from '@assets/icons/ICArrowLeft';
 import ICShop from '@assets/icons/ICShop';
 import ICLogout from '@assets/icons/ICLogout';
 import ICHome from '@assets/icons/ICHome';
 import ICProfile from '@assets/icons/ICProfile';
 import ICWallet from '@assets/icons/ICWallet';
-export {ICShop, ICLogout, ICHome, ICProfile, ICWallet};
+export {ICArrowLeft, ICShop, ICLogout, ICHome, ICProfile, ICWallet};
