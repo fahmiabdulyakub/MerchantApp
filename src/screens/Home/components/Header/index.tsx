a9e082ae0f26b0b3d8e643320c5e4a2c78fb3779
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-09-14 20:47:32 +0700

    handle Statusbar Home

--- src/screens/Home/components/Header/index.tsx
+++ src/screens/Home/components/Header/index.tsx
@@ -1,18 +1,14 @@
-import {StatusBar, View} from 'react-native';
+import {View} from 'react-native';
 import React, {memo} from 'react';
 import styles from '@screens/Home/components/Header/styles';
 import {Button} from '@components';
 import {ICBell} from '@assets/icons';
-import {COLORS} from '@constants/colors';
 
 const Header = () => {
   return (
-    <>
-      <StatusBar backgroundColor={COLORS.GREEN} barStyle="light-content" />
-      <View style={styles.container}>
-        <Button icon={<ICBell />} />
-      </View>
-    </>
+    <View style={styles.container}>
+      <Button icon={<ICBell />} />
+    </View>
   );
 };
 
