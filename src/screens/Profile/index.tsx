3ee22babb14b3ee62ba791544e2bdb38d8dbc0a9
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-09-16 01:26:09 +0700

    remove token

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,11 +1,21 @@
 import {View, ScrollView} from 'react-native';
-import React from 'react';
+import React, {useCallback} from 'react';
 import {styles} from '@screens/Profile/styles';
 import Header from '@screens/Profile/components/Header';
 import {Button} from '@components';
 import {ICLogout} from '@assets/icons';
+import {useEncryptedToken} from '@hooks';
+import {reset} from '@lib/navigation/utils';
+import {RouteNames} from '@lib/navigation/routes';
 
 const Profile = () => {
+  const {removeToken} = useEncryptedToken();
+
+  const handleLogout = useCallback(() => {
+    removeToken();
+    reset(RouteNames.SIGN_IN);
+  }, [removeToken]);
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.container}>
@@ -16,6 +26,7 @@ const Profile = () => {
         label="Logout"
         textStyle={styles.labelButton}
         style={styles.button}
+        onPress={handleLogout}
       />
     </View>
   );
