5721d78e2a7abffc55ecb6e03d62f1f6bed8b6c2
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-09-16 14:10:40 +0700

    fix bottom tab style

--- src/lib/navigation/config.ts
+++ src/lib/navigation/config.ts
@@ -1,15 +1,16 @@
 import {COLORS} from '@constants/colors';
 import {FONTS} from '@constants/fonts';
+import {isAndroid, isIOS} from '@utils/DeviceInfoUtils';
 
 export const MAIN_TABS_OPTIONS = {
   tabBarActiveTintColor: COLORS.GREEN,
   tabBarInactiveTintColor: COLORS.LIGHT_GREY,
-  tabBarStyle: {height: 65},
+  tabBarStyle: {height: isAndroid ? 65 : 85},
   tabBarLabelStyle: {
     fontFamily: FONTS.MontserratSemiBold,
     fontSize: 12,
     lineHeight: 19,
-    marginBottom: 8,
+    marginBottom: isIOS ? 0 : 8,
   },
   tabBarIconStyle: {marginTop: 13},
   tabBarHideOnKeyboard: true,
